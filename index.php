ac7cf0907a393c0cafd3b3666e39922ff9618241
ryom0624 <ryo.m0624@gmail.com>
2018-08-20 10:46:26 +0900

    1st commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>簡易掲示板</title>
+</head>
+<body>
+  <h1>簡易掲示板</h1>
+  <form class="" action="index.html" method="post">
+    message : <input type="text" name="message">
+    user : <input type="text" name="user">
+    <input type="submit" value="投稿">
+  </form>
+  <h2>投稿一覧 (0件) </h2>
+  <ul>
+    <li>ダミー投稿</li>
+  </ul>
+</body>
+</html>
