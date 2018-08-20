cd8622d33dd540702445092a924942d9842b759c
ryom0624 <ryo.m0624@gmail.com>
2018-08-20 11:59:33 +0900

    datファイルの読み込みと表示

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 
 $dataFile = 'bbs.dat';
 
+function h($s) {
+  return htmlspecialchars($s, ENT_QUOTES, 'UTF-8');
+}
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST' &&
   isset($_POST['message']) &&
   isset($_POST['user'])) {
@@ -23,10 +27,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' &&
 
     $fp = fopen($dataFile, 'a');
     fwrite($fp, $newData);
-    fclose($fp);
-  }
 
+    fclose($fp);
+
+  }
 }
+
+$posts  = file($dataFile, FILE_IGNORE_NEW_LINES);
+
+$posts = array_reverse($posts);
+
+
 
 ?>
 
@@ -46,9 +57,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' &&
     user : <input type="text" name="user">
     <input type="submit" value="投稿">
   </form>
-  <h2>投稿一覧 (0件) </h2>
+  <h2>投稿一覧 (<?php echo count($posts) ?>件) </h2>
   <ul>
+    <?php if (count($posts)) : ?>
+      <?php foreach ($posts as $post) : ?>
+        <?php list($message, $user, $postedAt) = explode("\t", $post); ?>
+          <li><?php echo h($message); ?> (<?php echo h($user); ?>) - <?php echo h($postedAt); ?></li>
+      <?php endforeach; ?>
+    <?php else : ?>
     <li>まだ投稿はありません。</li>
+    <?php endif; ?>
   </ul>
 </body>
 </html>
